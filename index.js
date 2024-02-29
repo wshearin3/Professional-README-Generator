039163dd802b90e7ad28990c86ea487462667f5f
Trey <trey.shearin3@gmail.com>
2024-02-29 10:56:09 -0500

    added variables requiring inquirer and fs

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 // TODO: Include packages needed for this application
 
+const inquirer = require('inquirer');
+const fs = require('fs');
+
 // TODO: Create an array of questions for user input
 const questions = [];
 
