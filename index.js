88f40c9f0fa3012808f0f792ab265945085ca121
Trey <trey.shearin3@gmail.com>
2024-02-29 11:03:09 -0500

    added question to capture project title

--- index.js
+++ index.js
@@ -4,7 +4,13 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        message: "What is the name of your project?",
+        name: "projectTitle"
+    },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
