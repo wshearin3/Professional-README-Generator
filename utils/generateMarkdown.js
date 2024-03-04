fc28caef8cdc8fc0f92bb869783b7799cbff4f4d
Trey <trey.shearin3@gmail.com>
2024-03-04 17:55:34 -0500

    changed returns for license badges to only include license badge and not links

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,16 +1,16 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
-  if (answers.license == "No License") {
+  if (license == "No License") {
     return "";
-  } else if (answers.license == "MIT") {
-    return "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
-  } else if (answers.license == "Apache") {
-    return "[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)";
-  } else if (answers.license == "BSD") {
-    return "[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)";
-  } else if (answers.license == "GPL") {
-    return "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)";
+  } else if (license == "MIT") {
+    return "![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)";
+  } else if (license == "Apache") {
+    return "![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)";
+  } else if (license == "BSD") {
+    return "![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)";
+  } else if (license == "GPL") {
+    return "![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)";
   }
 }
 
@@ -23,10 +23,11 @@ function renderLicenseLink(license) {}
 function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
-function generateMarkdown(answers) {
+function generateMarkdown(answers, license) {
+  let licenseBadge = renderLicenseBadge(answers.license);
   console.log(answers);
   return `# ${answers.projectTitle}
-  
+  ${licenseBadge}
 
 `;
 }
