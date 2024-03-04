a811d884d2e0357d8e7fc806e52f634ec326b547
Trey <trey.shearin3@gmail.com>
2024-03-04 17:39:45 -0500

    created if statement to return license badges based on user statement in generateMarkdown

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,18 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (answers.license == "No License") {
+    return "";
+  } else if (answers.license == "MIT") {
+    return "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
+  } else if (answers.license == "Apache") {
+    return "[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)";
+  } else if (answers.license == "BSD") {
+    return "[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)";
+  } else if (answers.license == "GPL") {
+    return "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)";
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
