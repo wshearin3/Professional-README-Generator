bf8ee58dcb9cf066ad0800e465b40ed0b5a9ec1a
Trey <trey.shearin3@gmail.com>
2024-03-04 16:19:34 -0500

    added constant to require Markdown file

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 const inquirer = require('inquirer');
 const fs = require('fs');
+const generateMarkdown = require( "./utils/generateMarkdown");
 
 // TODO: Create an array of questions for user input
 const questions = [
