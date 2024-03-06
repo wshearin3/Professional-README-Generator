450c21506c897876533f4e5f004dea473491f874
Trey <trey.shearin3@gmail.com>
2024-03-06 11:17:27 -0500

    updated questions sections with instructions on how to contact, updated table of context syntax to link to sections when readme is produced

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -43,13 +43,13 @@ function generateMarkdown(answers, license) {
   ${licenseBadge}
 
   ## Table of Contents
-* Description
-* Installation
-* Usage
-* License
-* Contributing
-* Tests
-* Questions
+* [Description](#description)
+* [Installation](#installation)
+* [Usage](#usage)
+* [License](#license)
+* [Contributing](#contributing)
+* [Tests](#tests)
+* [Questions](#questions)
 
  ## Description
 ${answers.description}
@@ -71,7 +71,11 @@ ${answers.contributions}
 
  ## Questions
  
- github.com/${answers.username}
+ (github.com/${answers.username})
+
+ Please reach out to me with any additional questions you may have using my email address provided below:
+
+ (${answers.email})
  
 
 `;
